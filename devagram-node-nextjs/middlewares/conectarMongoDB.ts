95bc249446842bb7c4385a83bd6699f12fab200c
Felipe Taleires <jfelipetaleires@hotmail.com>
2022-08-02 21:03:20 -0300

    configuraçao de banco de dados

---
+++ devagram-node-nextjs/middlewares/conectarMongoDB.ts
@@ -0,0 +1,30 @@
+import type {NextApiRequest, NextApiResponse, NextApiHandler} from 'next';
+import mongoose from 'mongoose';
+import type { RespostaPadraoMsg } from '../types/RespostaPadraoMsg';
+
+export const conectarMongoDB = (handler : NextApiHandler) =>
+    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+
+
+        //verificar se o banco ja esta conectado, se estiver seguir para o endpoint ou próximo midleware
+        if(mongoose.connections[0].readyState){
+            return handler(req, res);
+        }
+
+        //ja que nao esta conectado vamos conectar
+        //obter a variavel de ambiente preenchida do env
+        const {DB_CONEXAO_STRING} = process.env;
+
+        // se a env estiver vazia, abortar o uso do sistema e avisar o programador
+        if(!DB_CONEXAO_STRING){
+            return res.status(500).json({erro : 'ENV de configuracao do banco nao informado'});
+        }
+
+        mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
+        mongoose.connection.on('error', error => console.log(`Ocorreu erro ao conectar no banco: ${error}`));
+        await mongoose.connect(DB_CONEXAO_STRING);
+
+
+        // agora posso seguir, pois estou conectado ao banco
+        return handler(req, res);
+    }
--- devagram-node-nextjs/pages/api/login.ts
+++ devagram-node-nextjs/pages/api/login.ts
@@ -1,17 +1,22 @@
 import type {NextApiRequest, NextApiResponse} from "next";
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
 
-export default (
+const endpointLogin = (
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse<RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
         
         if(login === 'admin@admin.com' && 
         senha === 'Admin@123'){
-            res.status(200).json({msg: 'Usuário autenticado com sucesso'});
+           return res.status(200).json({msg: 'Usuário autenticado com sucesso'});
         }
         return res.status(400).json({erro: 'Usuario ou senha não encontrado'});
     }
     return res.status(405).json({erro: 'Método informado não é válido'});
 }
+
+//Primeiro vai para o middleware, para depois tentar executar o endpoint
+export default conectarMongoDB(endpointLogin);
