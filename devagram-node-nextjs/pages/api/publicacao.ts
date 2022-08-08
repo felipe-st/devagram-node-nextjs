98069cb6184d2d0cac9cfde1048b34665d9a3e49
Felipe Taleires <jfelipetaleires@hotmail.com>
2022-08-08 18:46:31 -0300

    publicacao

--- devagram-node-nextjs/pages/api/publicacao.ts
+++ devagram-node-nextjs/pages/api/publicacao.ts
@@ -4,12 +4,19 @@ import nc from 'next-connect';
 import { upload, uploadImagemCosmic } from "../../services/uploadImagemCosmic";
 import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
 import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
+import { PublicacaoModel } from "../../models/PublicacaoModel";
+import { UsuarioModel } from "../../models/UsuarioModels";
 
 const handler = nc()
     .use(upload.single('file'))
     .post(async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
         
         try{
+            const {userId} = req.query;
+            const usuario = await UsuarioModel.findById(userId);
+            if(!usuario){
+                return res.status(400).json({erro: 'Usuario nao encontrado'});
+            }
             if(!req || !req.body){
                 return res.status(400).json({erro: 'Parametros de entrada nao informados'});
             }
@@ -19,11 +26,20 @@ const handler = nc()
                 return res.status(400).json({erro: 'Descricao nao e valida'});
             }
     
-            if(!req.file){
+            if(!req.file || !req.file.originalname){
                 return res.status(400).json({erro: 'Imagem e obrigatoria'});
             }
-            
-            return res.status(200).json({msg: 'Publicacao esta valida'});
+
+            const image = await uploadImagemCosmic(req);
+            const publicacao = {
+                idUsuario : usuario._id,
+                descricao,
+                foto : image.media.url,
+                data : new Date()
+            }
+
+            await PublicacaoModel.create(publicacao);            
+            return res.status(200).json({msg: 'Publicacao criada com sucesso'});
 
         }catch(e){
             console.log(e);
