ce8475e38978bac80eca1a4332c2177415f1d1d4
Felipe Taleires <jfelipetaleires@hotmail.com>
2022-08-10 19:58:36 -0300

    conclusao da home de um usuario especifico

--- devagram-node-nextjs/pages/api/pesquisa.ts
+++ devagram-node-nextjs/pages/api/pesquisa.ts
@@ -8,8 +8,15 @@ const pesquisaEndpoint = async (req: NextApiRequest, res: NextApiResponse<Respos
 
     try{
         if(req.method === 'GET'){
-
-            const {filtro} = req.query;
+            if(req?.query?.id){
+                const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id);
+                if(!usuarioEncontrado){
+                    return res.status(400).json({erro: 'Usuario nao encontrado'});
+                }
+                usuarioEncontrado.senha = null;
+                return res.status(200).json(usuarioEncontrado);
+            }else{
+                const {filtro} = req.query;
 
             if(!filtro || filtro.length < 2){
                 return res.status(400).json({erro: 'Favor informar pelo menos 2 caraceres para a busca'});
@@ -21,7 +28,7 @@ const pesquisaEndpoint = async (req: NextApiRequest, res: NextApiResponse<Respos
             });
 
             return res.status(200).json(usuariosEncontrados);
-
+            }
         }
         return res.status(405).json({erro: 'Metodo informado nao e valido:'});
     }catch(e){
